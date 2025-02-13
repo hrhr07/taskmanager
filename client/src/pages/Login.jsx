5b05352fbc57ca4c6d9c531b9efb7b4733121671
Hector Hernandez <hectorrhr07@gmail.com>
2025-02-13 09:46:14 -0600

    creacion del UI del Loginpage

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,101 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Textbox from "../components/Textbox";
+import Button from "../components/Button";
 
 export default function Login() {
+  const { user } = "";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  const submitHandler = async (data) => {
+    console.log("submit");
+  };
+
+  useEffect(() => {
+    user && navigate("/dashboard");
+  }, [user]);
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+      <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+        {/* left side */}
+        <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
+            <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600">
+              Planifica mejor, trabaja más inteligente
+            </span>
+            <p className="text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-slate-500">
+              <span>
+                Administrador de tareas{" "}
+                <span className="text-blue-500">Planix</span>
+              </span>
+            </p>
+
+            <div className="cell">
+              <div className="circle rotate-in-up-left"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center">
+          <form
+            onSubmit={handleSubmit(submitHandler)}
+            className="form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14"
+          >
+            <div className="">
+              <p className="text-slate-500 text-3xl font-bold text-center">
+                ¡Bienvenido de nuevo!
+              </p>
+              <p className="text-center text-base text-gray-700 ">
+                Mantén tus credenciales seguras.
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-y-5">
+              <Textbox
+                placeholder="email@ejemplo.com"
+                type="email"
+                name="email"
+                label="Correo Electrónico"
+                className="w-full rounded-full"
+                register={register("email", {
+                  required: "¡Se requiere Correo Electrónico!",
+                })}
+                error={errors.email ? errors.email.message : ""}
+              />
+              <Textbox
+                placeholder="tu contraseña"
+                type="password"
+                name="password"
+                label="Contraseña"
+                className="w-full rounded-full"
+                register={register("password", {
+                  required: "¡Se requiere contraseña!",
+                })}
+                error={errors.password ? errors.password.message : ""}
+              />
+
+              <span className="text-sm text-gray-500 hover:text-blue-500 hover:underline cursor-pointer">
+                ¿Olvidaste tu contraseña?
+              </span>
+
+              <Button
+                type="submit"
+                label="Ingresar"
+                className="w-full h-10 bg-blue-500 text-white rounded-full"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
